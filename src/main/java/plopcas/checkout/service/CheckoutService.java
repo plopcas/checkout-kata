84ec9cf85ac155fdbb0da22311386bc99b6378db
Pedro Lopez <plopcas@gmail.com>
2018-07-28 00:34:19 +0100

    GRN Add validation and filter out nulls in checkout service

--- src/main/java/plopcas/checkout/service/CheckoutService.java
+++ src/main/java/plopcas/checkout/service/CheckoutService.java
@@ -1,6 +1,8 @@
 package plopcas.checkout.service;
 
+import java.util.Objects;
 import java.util.stream.Collectors;
+import plopcas.checkout.exception.CartNotValidException;
 import plopcas.checkout.model.Cart;
 import plopcas.checkout.model.Item;
 import plopcas.checkout.model.Price;
@@ -8,9 +10,15 @@ import plopcas.checkout.model.Price;
 public class CheckoutService {
 
   public Price checkout(Cart cart) {
-    Integer total = cart.getItems().stream().map(Item::getPrice)
+
+    if (cart == null) {
+      throw new CartNotValidException("Cart is null");
+    }
+
+    Integer total = cart.getItems().stream().filter(Objects::nonNull).map(Item::getPrice)
         .collect(Collectors.summingInt(Integer::intValue));
     return new Price(total);
+
   }
 
 }
