c0c50893f7fd6aa30fd413e2e29615e238682836
Pedro Lopez <plopcas@gmail.com>
2018-07-28 02:33:06 +0100

    GRN Add complex checkout logic

--- src/main/java/plopcas/checkout/service/CheckoutService.java
+++ src/main/java/plopcas/checkout/service/CheckoutService.java
@@ -1,9 +1,11 @@
 package plopcas.checkout.service;
 
+import java.util.Map;
 import java.util.Objects;
 import java.util.stream.Collectors;
 import plopcas.checkout.exception.CartNotValidException;
 import plopcas.checkout.model.Cart;
+import plopcas.checkout.model.Discount;
 import plopcas.checkout.model.Item;
 import plopcas.checkout.model.Price;
 
@@ -14,9 +16,37 @@ public class CheckoutService {
       throw new CartNotValidException("Cart is null");
     }
 
-    Integer total = cart.getItems().stream().filter(Objects::nonNull).map(Item::getPrice)
-        .collect(Collectors.summingInt(Integer::intValue));
-    return new Price(total);
+    Integer total = calculateTotal(cart);
+    Integer discount = calculateDiscount(cart);
+
+    return new Price(total - discount);
   }
 
+  private Integer calculateTotal(Cart cart) {
+    return cart.getItems().stream()
+        .filter(Objects::nonNull)
+        .map(Item::getPrice)
+        .collect(Collectors.summingInt(Integer::intValue));
+  }
+
+  private Integer calculateDiscount(Cart cart) {
+    Map<Item, Long> countMap = cart.getItems().stream()
+        .filter(Objects::nonNull)
+        .collect(Collectors.groupingBy(item -> item, Collectors.counting()));
+
+    return countMap.entrySet().stream().map(entry -> {
+
+      Item item = entry.getKey();
+      Long count = entry.getValue();
+      Discount discount = item.getDiscount();
+
+      if (discount == null) {
+        return 0;
+      } else {
+        Long times = count / discount.getCount();
+        return discount.getValue() * times.intValue();
+      }
+
+    }).collect(Collectors.summingInt(Integer::intValue));
+  }
 }
