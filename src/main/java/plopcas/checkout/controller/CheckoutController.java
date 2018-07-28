95846774c36e9b7076f05e6d80122930a9f75cc4
Pedro Lopez <plopcas@gmail.com>
2018-07-28 15:40:17 +0100

    GRN Add checkout controller handling

--- src/main/java/plopcas/checkout/controller/CheckoutController.java
+++ src/main/java/plopcas/checkout/controller/CheckoutController.java
@@ -1,6 +1,7 @@
 package plopcas.checkout.controller;
 
 import java.util.Scanner;
+import plopcas.checkout.model.Cart;
 import plopcas.checkout.model.Result;
 import plopcas.checkout.service.CheckoutService;
 import plopcas.checkout.service.ItemService;
@@ -8,6 +9,7 @@ import plopcas.checkout.service.ScannerService;
 
 public class CheckoutController {
 
+  private static final String END = "end";
 
   private Scanner sc;
   private ItemService itemService;
@@ -23,6 +25,42 @@ public class CheckoutController {
   }
 
   public Result handleCheckoutTransaction() {
-    return null;
+    Cart cart = new Cart();
+
+    String itemId = first(cart);
+
+    scannerService.scan(itemService.find(itemId), cart);
+
+    itemId = next(cart);
+    while (!END.equalsIgnoreCase(itemId.trim())) {
+      scannerService.scan(itemService.find(itemId), cart);
+      itemId = next(cart);
+    }
+
+    Result result = checkoutService.checkout(cart);
+    end(result);
+
+    return result;
+  }
+
+  private String first(Cart cart) {
+    System.out.println("== Checkout application ==");
+    System.out.println("Scan first item to begin: ");
+    return sc.nextLine();
+  }
+
+  private String next(Cart cart) {
+    System.out.println(cart);
+    System.out.println("More items? (Type END to proceed to checkout)");
+    return sc.nextLine();
+  }
+
+  private void end(Result result) {
+    System.out.println("Total:\n " + result.getTotal());
+    if (result.getDiscount() != 0) {
+      System.out.println(" - " + result.getDiscount() + " (discounts)");
+    }
+    System.out.println("To pay: " + result.getToPay());
   }
 }
+
