1d6aac58ff1e3c60a821a88553547c4f6b470b17
Pedro Lopez <plopcas@gmail.com>
2018-07-28 13:27:35 +0100

    GRN Add hard-coded pricing rules

--- src/main/java/plopcas/checkout/service/PricingService.java
+++ src/main/java/plopcas/checkout/service/PricingService.java
@@ -1,6 +1,8 @@
 package plopcas.checkout.service;
 
 import java.io.File;
+import plopcas.checkout.model.Discount;
+import plopcas.checkout.model.Item;
 import plopcas.checkout.model.PricingRules;
 
 public class PricingService {
@@ -12,7 +14,12 @@ public class PricingService {
   }
 
   public PricingRules getPricingRules() {
-    return null;
+    PricingRules pricingRules = new PricingRules();
+    pricingRules.put("A", new Item("A", 50, new Discount(3, 20)));
+    pricingRules.put("B", new Item("B", 30, new Discount(2, 15)));
+    pricingRules.put("C", new Item("C", 20));
+    pricingRules.put("D", new Item("D", 15));
+    return pricingRules;
   }
 
 }
